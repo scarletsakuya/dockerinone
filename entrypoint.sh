b54811306f9adfcb9058d4d80a77e9c616d36753
scarletsakuya <31232410+scarletsakuya@users.noreply.github.com>
2018-09-06 19:17:55 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,6 +18,8 @@ sed -i "s/\[KCP_UPLINK\]/${KCP_UPLINK}/g" /etc/v2ray/config.json
 sed -i "s/\[KCP_DOWNLINK\]/${KCP_DOWNLINK}/g" /etc/v2ray/config.json
 sed -i "s/\[KCP_READBUFF\]/${KCP_READBUFF}/g" /etc/v2ray/config.json
 sed -i "s/\[KCP_WRITEBUFF\]/${KCP_WRITEBUFF}/g" /etc/v2ray/config.json
+sed -i "s/\[]/${TOKEN}/g" /root/.ehforwarderbot/profiles/default/bluestet.telegram\config.yaml
+sed -i "s/\[]/${ADM}/g" /root/.ehforwarderbot/profiles/default/bluestet.telegram\config.yaml
 
 for i in "$@"; do
 	bash $i
